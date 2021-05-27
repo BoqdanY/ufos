b43217a3228bc9d931aaab3d96ce1cd592d48920
BoqdanY <65012653+BoqdanY@users.noreply.github.com>
2021-05-27 22:29:52 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+let p = document.getElementById("pap");
+let a = 0;
+
+window.onscroll = function(){scrollFunction()}
+window.onscroll = function(){scrolls()}
+
+//Випливаюча підказка
+function b1(){
+	p.innerHTML = "Data source" + "(On the Moon): " + '<a href="https://google.com" target="_blank">http://google.com</a>';
+}
+
+function b2(){
+	p.innerHTML = "Data source" + "(On the Earth): " + '<a href="https://youtube.com.com" target="_blank">http://youtube.com</a>';
+}
+
+function b3(){
+	p.innerHTML = "Data source" + "(On the Space): " + '<a href="https://st2.depositphotos.com/4164031/7029/i/600/depositphotos_70298385-stock-photo-deep-space.jpg" target="_blank">http://space.com</a>';
+}
+
+
+//Header зменшується
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.getElementById("header").style.padding = "0.5% 0";
+  } else {
+    document.getElementById("header").style.padding = "1.3% 0";
+  }
+}
+
+//Зміна menu
+function change(){
+	let link = document.getElementById("link1");
+
+	if (a == 0) {
+		a += 1;
+		link1.href = "assets/css/dark-style.css";
+	}else{
+		a -= 1;
+		link1.href = "assets/css/style.css";
+	}
+	
+}
+
+function scrolls(){
+	if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+		document.getElementById("cover1").classList.add("anim");
+		document.getElementById("cover2").classList.add("anim");
+		document.getElementById("cover3").classList.add("anim");
+	}
+}
